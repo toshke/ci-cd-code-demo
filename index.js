8d0a82df199424dd8043f88ef73bd9b048bd8f8b
Nikola Tosic <tosicnikola10@gmail.com>
2017-12-20 19:13:45 +0100

    hello from bi

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello from base2 and SimpleTask!'));
+app.get('/', (req, res) => res.send('Pozdravi iz Business Incubatora!!'));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
